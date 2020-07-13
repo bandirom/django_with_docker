6e60e50c7cc7a2d01e57053f8b09085435050383
bandirom <bandirom@ukr.net>
2020-07-13 14:15:59 +0300

    rename folders scripts -> js && styles -> css

--- web/src/urls.py
+++ web/src/urls.py
@@ -28,6 +28,6 @@ handler404 = 'app.views.custom_handler404'
 handler500 = 'app.views.custom_handler500'
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
